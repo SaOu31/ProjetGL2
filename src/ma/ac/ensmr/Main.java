f21acb7ad72bb9d5b1ee10a979476b6e2f4da849
salmaouardi <salmaouardi@localhost>
2024-10-31 21:24:24 +0100

    first commit

---
+++ src/ma/ac/ensmr/Main.java
@@ -0,0 +1,16 @@
+package ma.ac.ensmr;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		
+		System.out.print("hello ensmr !");
+
+	}
+
+}
